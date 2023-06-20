fc323dd02146784566f0000f9c7ab088d099209c
MikaeloBlanco <117371666+MikaeloBlanco@users.noreply.github.com>
2023-06-20 19:56:57 +0200

    Java Tests 1

---
+++ src/test/java/com/cpfifp/grijander/parkingfinal/JavaTests.java
@@ -0,0 +1,12 @@
+package com.cpfifp.grijander.parkingfinal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaTests{
+
+    @Test
+    
+
+}
